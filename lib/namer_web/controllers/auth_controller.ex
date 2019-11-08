8bfc10010b9dcd6a722ac04ecd4c310f59d0c78c
Evan Dancer <evancdancer@gmail.com>
2019-11-07 21:46:33 -0800

    Create and sign in user after auth

--- lib/namer_web/controllers/auth_controller.ex
+++ lib/namer_web/controllers/auth_controller.ex
@@ -1,28 +1,55 @@
 defmodule NamerWeb.AuthController do
   use NamerWeb, :controller
 
-  def request(conn, %{"provider" => provider}) do
-    redirect(conn, external: authorize_url!(provider))
+  alias Namer.Accounts
+
+  def request(conn, _) do
+    redirect(conn, external: authorize_url!())
   end
 
-  def callback(conn, %{"provider" => provider, "code" => code}) do
-    client = get_token!(provider, code)
-    _user = get_athlete!(client)
-    conn
-    |> put_flash(:error, "Authentication failed for #{provider}")
-    |> redirect(to: Routes.page_path(conn, :index))
+  def callback(conn, %{"code" => code}) do
+    client = get_token!(code)
+    user_params = user_attrs(client)
+
+    case Accounts.get_user_by_uid(user_params[:strava_uid]) do
+      {:ok, user} -> sign_in_and_redirect(conn, user)
+      _ -> create_user_and_redirect(conn, user_params)
+    end
   end
 
-  defp authorize_url!("strava") do
+  defp authorize_url!() do
     scope = "read,activity:read_all,activity:write"
     Strava.Auth.authorize_url!(scope: scope)
   end
 
-  defp get_token!("strava", code) do
+  defp get_token!(code) do
     Strava.Auth.get_token!(code: code, grant_type: "authorization_code")
   end
 
-  defp get_athlete!(client) do
-    Strava.Auth.get_athlete!(client)
+  defp user_attrs(%{token: token} = client) do
+    user = Strava.Auth.get_athlete!(client)
+    %{
+      avatar: user.profile,
+      access_token: token.access_token,
+      name: "#{user.firstname} #{user.lastname}",
+      refresh_token: token.refresh_token,
+      strava_uid: "#{user.id}"
+    }
+  end
+
+  defp sign_in_and_redirect(conn, user) do
+    conn
+    |> put_session(:user_id, user.id)
+    |> redirect(to: Routes.page_path(conn, :index))
+  end
+
+  defp create_user_and_redirect(conn, user_params) do
+    case Accounts.create_user(user_params) do
+      {:ok, user} -> sign_in_and_redirect(conn, user)
+      _ ->
+        conn
+        |> put_flash(:error, "Authentication failed")
+        |> redirect(to: Routes.page_path(conn, :index))
+    end
   end
 end
