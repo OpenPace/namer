5ef3f9451128ee0057e0322a96e4836c0d90dc32
Evan Dancer <evancdancer@gmail.com>
2019-11-08 07:32:02 -0800

    Fix tests

--- test/namer_web/controllers/page_controller_test.exs
+++ test/namer_web/controllers/page_controller_test.exs
@@ -1,8 +1,8 @@
 defmodule NamerWeb.PageControllerTest do
   use NamerWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  test "GET /namer", %{conn: conn} do
+    conn = get(conn, "/namer")
+    assert html_response(conn, 200) =~ "Connect to Strava"
   end
 end
