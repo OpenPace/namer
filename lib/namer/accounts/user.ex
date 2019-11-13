ec1c43ac30d59ba87a272ebb7957b0bc07e2d36e
Evan Dancer <evancdancer@gmail.com>
2019-11-12 20:53:11 -0800

    Add users prefs

--- lib/namer/accounts/user.ex
+++ lib/namer/accounts/user.ex
@@ -6,6 +6,17 @@ defmodule Namer.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Namer.Accounts.UserPrefs
+
+  @required_fields ~w(
+    name
+    avatar
+    strava_uid
+    access_token
+    refresh_token
+  )a
+  @optional_fields ~w()a
+
   schema "users" do
     field :access_token, :string
     field :avatar, :string
@@ -13,13 +24,15 @@ defmodule Namer.Accounts.User do
     field :refresh_token, :string
     field :strava_uid, :string
 
+    has_one :user_prefs, UserPrefs
+
     timestamps()
   end
 
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:name, :avatar, :strava_uid, :access_token, :refresh_token])
-    |> validate_required([:name, :avatar, :strava_uid, :access_token, :refresh_token])
+    |> cast(attrs, @required_fields ++ @optional_fields)
+    |> validate_required(@required_fields)
   end
 end
