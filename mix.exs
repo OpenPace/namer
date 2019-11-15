0469fe0285d646eb84078e893941d44830651915
Evan Dancer <evancdancer@gmail.com>
2019-11-14 20:21:29 -0800

    Add test coverage with coveralls

--- mix.exs
+++ mix.exs
@@ -10,7 +10,9 @@ defmodule Namer.MixProject do
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
     ]
   end
 
@@ -45,7 +47,8 @@ defmodule Namer.MixProject do
       {:plug_cowboy, "~> 2.0"},
       {:strava, "~> 1.0"},
       {:httpoison, "~> 1.5"},
-      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test}
     ]
   end
 
