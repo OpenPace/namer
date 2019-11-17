1fdf9b11644c80128cad5f4fe603366ac97e7bef
Evan Dancer <evancdancer@gmail.com>
2019-11-17 14:43:28 -0800

    Add field for imperial to user prefs

---
+++ priv/repo/migrations/20191117224226_add_imperial_to_user_prefs.exs
@@ -0,0 +1,9 @@
+defmodule Namer.Repo.Migrations.AddImperialToUserPrefs do
+  use Ecto.Migration
+
+  def change do
+    alter table(:user_prefs) do
+      add :imperial, :boolean, default: false, null: true
+    end
+  end
+end
