f0e7734b348c60d92a227676a862b11d63246a54
Evan Dancer <evancdancer@gmail.com>
2019-11-17 10:03:54 -0800

    Add google tag manager

--- config/config.exs
+++ config/config.exs
@@ -22,6 +22,8 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :namer, gtm_id: System.get_env("GTM_ID")
+
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
