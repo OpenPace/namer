118f31dee7820022a212a31181b9a76dd584c945
Evan Dancer <evancdancer@gmail.com>
2019-12-17 20:48:55 -0800

    Limit retries to 3 times

--- lib/namer/renamer_job.ex
+++ lib/namer/renamer_job.ex
@@ -1,13 +1,13 @@
 defmodule Namer.RenamerJob do
   @moduledoc """
-  Basic job to retry naming 10 times.
+  Basic job to retry naming 3 times.
   """
   require Logger
 
   alias Namer.Accounts
   alias Namer.ActivityRenamer
 
-  def perform(strava_uid, activity_id, retry \\ 10)
+  def perform(strava_uid, activity_id, retry \\ 3)
   def perform(_, _, 0), do: :ignore
   def perform(strava_uid, activity_id, retry) do
     Logger.info("Starting job for #{strava_uid} for activity #{activity_id}")
