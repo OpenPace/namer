ced1b121d079437c4d85340dfba90bc3410df307
Evan Dancer <evancdancer@gmail.com>
2019-12-23 17:18:02 -0500

    Add view tests

--- test/namer_web/views/page_view_test.exs
+++ test/namer_web/views/page_view_test.exs
@@ -1,3 +1,9 @@
 defmodule NamerWeb.PageViewTest do
   use NamerWeb.ConnCase, async: true
+
+  alias NamerWeb.PageView
+
+  test "title includes emoji" do
+    assert PageView.title(%{}, %{}) =~ "Emojify your Strava Activities"
+  end
 end
