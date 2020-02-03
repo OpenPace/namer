d021b6b050fa05550fa1090837d86f61df257959
Evan Dancer <evancdancer@gmail.com>
2020-02-02 20:45:38 -0800

    Add tests for accounts.ex

--- test/namer/accounts_test.exs
+++ test/namer/accounts_test.exs
@@ -1,3 +1,27 @@
 defmodule Namer.AccountsTest do
   use Namer.DataCase
+
+  alias Namer.Accounts
+
+  import Namer.Factory
+
+  describe "get_user_by_uid/1" do
+    setup [:create_user]
+
+    test "works with integer" do
+      assert user = Accounts.get_user_by_uid(1234)
+      assert user.id != nil
+      assert user.strava_uid == "1234"
+    end
+
+    test "works with string" do
+      assert user = Accounts.get_user_by_uid("1234")
+      assert user.id != nil
+      assert user.strava_uid == "1234"
+    end
+  end
+
+  defp create_user(_) do
+    {:ok, user: insert(:user, strava_uid: "1234")}
+  end
 end
